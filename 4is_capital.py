18d83d29075d563acc417cf647bbaa49b9e124df
vladimir-kap <vladimir362616@gmail.com>
2025-03-04 12:50:08 +0300

    fixed 4. used mock variable and fixed loop iteration.

--- 4is_capital.py
+++ 4is_capital.py
@@ -7,27 +7,22 @@
 # "coding", return true
 
 x = input("enter a string to check for correct use of capitalization: ")
-firstCapi = 0
-anyCapi = 0
-    
+
 def capitalization():
+    capiMock = 0
+    if x.isupper():
+        print(f"1.CORRECT - given string {x} is uppercase")
+    if x.islower():
+        print(f"2.CORRECT - given string {x} is lowercase")
     for i in x:
         #used old code from learning py to recall how to use slices in this case
         if x[0].isupper() and x[1:].islower():
-            firstCapi = 1
-    if firstCapi == 1:
-        print(f"1.CORRECT - given string: {x} the first letter is {x[0]} and others is lowercase {x[0].isupper()}")
-    if x.islower():
-        print(f"2.CORRECT - given string {x} is lowercase")
-    if x.isupper():
-        print(f"3.CORRECT - given string {x} is uppercase")
+            capiMock  = 1
+            print(f"3.CORRECT - given string {x} is a name")
+            break
     for i in x:
-        if x[0].isupper() and x[1:].islower():
-            if x[1:].isupper():
-                anyCapi = 1
-    if anyCapi == 1:
-        print(f"4.INCORRECT - given string: {x} not a Name or lowercase or uppercase")
-
-    pass
-
+        if not x.isupper() and not x.islower() and capiMock != 1:
+            print(f"4.INCORRECT - given string {x} is brokenCase")
+            break
+#also googled for break usage for printing result only once, and not for every loop iteration
 capitalization()
